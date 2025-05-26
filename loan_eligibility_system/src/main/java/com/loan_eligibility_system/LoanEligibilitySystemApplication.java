c11727c71432cd33bcb38a73aa97547600b78987
Mohamed-Aslam-dev <aslam93615@gmail.com>
2025-05-26 16:25:21 +0530

    commit xml

--- loan_eligibility_system/src/main/java/com/loan_eligibility_system/LoanEligibilitySystemApplication.java
+++ loan_eligibility_system/src/main/java/com/loan_eligibility_system/LoanEligibilitySystemApplication.java
@@ -8,6 +8,7 @@ public class LoanEligibilitySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanEligibilitySystemApplication.class, args);
+		System.out.println("Testedok");
 	}
 
 }
