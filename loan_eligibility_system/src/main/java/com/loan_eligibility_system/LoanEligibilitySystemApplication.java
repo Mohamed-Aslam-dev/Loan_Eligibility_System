3daf565eead109fb8f59455cfa5868c89ecb0f36
Mohamed-Aslam-dev <aslam93615@gmail.com>
2025-05-26 15:10:00 +0530

    commit

---
+++ loan_eligibility_system/src/main/java/com/loan_eligibility_system/LoanEligibilitySystemApplication.java
@@ -0,0 +1,13 @@
+package com.loan_eligibility_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligibilitySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligibilitySystemApplication.class, args);
+	}
+
+}
---
+++ loan_eligibility_system/src/test/java/com/loan_eligibility_system/LoanEligibilitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_eligibility_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligibilitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
